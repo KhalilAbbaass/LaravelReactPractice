8caf2bf21aca236c4ba2fc7cfd7b3ffa60cb2e9d
Khalil Abbas <103119609+KhalilAbbaass@users.noreply.github.com>
2023-03-20 15:04:30 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -46,6 +46,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="App">
+      
          <div>
           <ContextProvider>
             <RouterProvider router={router} />
