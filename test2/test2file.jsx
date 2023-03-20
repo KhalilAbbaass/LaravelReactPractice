f7fbe2b911c4932987f9f4a7dbe96a18cc909fb0
Khalil Abbas <103119609+KhalilAbbaass@users.noreply.github.com>
2023-03-20 13:34:15 +0200

    delette

--- test2/test2file.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const test2file = () => {
-  return (
-    <div>test2file</div>
-  )
-}
-
-export default test2file
--- test3/test4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const test4 = () => {
-  return (
-    <div>test4</div>
-  )
-}
-
-export default test4
